40bafe80ee45d4736244b77e55cb62f81a7eca67
marydotdev <mary@Marys-Mac-mini.local>
2023-05-19 10:08:34 -0400

    layout

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,14 @@
+<script>
+  import ThemeToggle from '$lib/ThemeToggle.svelte';
+</script>
+
+<nav class="z-10 bg-zinc-50 dark:bg-zinc-900 border-b-2 border-zinc-900 dark:border-zinc-50">
+	<div class="w-full p-4 flex items-center justify-between">
+		<div class="w-fit">
+			<a href="/" class="font-medium text-lg">hack</a>
+		</div>
+		<div class="w-fit h-6">
+			<ThemeToggle />
+		</div>
+	</div>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,17 @@
 <script>
-	import '../app.css';
-	import ThemeToggle from '../lib/ThemeToggle.svelte';
+	import Footer from '$lib/Footer.svelte';
+  import Nav from '$lib/Nav.svelte';
+  import '../app.css';
 </script>
 
-<div class="w-full h-screen font-spacemono flex flex-col justify-between">
-	<div>
-		<nav class="z-10 sticky top-0 bg-zinc-50 dark:bg-zinc-900 border-b-2 border-zinc-900 dark:border-zinc-50">
-			<div class="w-full p-4 flex items-center justify-between">
-				<div class="w-fit">
-					<a href="/" class="font-medium text-lg">hack.dev</a>
-				</div>
-				<div class="w-fit h-6">
-					<ThemeToggle />
-				</div>
-			</div>
-		</nav>
-		<slot />
-	</div>
-	<footer class="z-10 sticky bottom-0 bg-zinc-50 dark:bg-zinc-900 border-t-2 border-zinc-900 dark:border-zinc-50">
-		<div class="w-full p-4 flex items-center justify-center">made by mary</div>
-	</footer>
+<div class="overflow-hidden relative w-full h-screen font-spacemono flex flex-col">
+  <div>
+    <Nav />
+  </div>
+  <div class="scrollbar overflow-y-scroll">
+    <slot />
+  </div>
+  <div>
+    <Footer />
+  </div>
 </div>
